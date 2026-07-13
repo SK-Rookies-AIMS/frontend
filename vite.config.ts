b6a35a76abbc5531edf8ed04c09d9bbb0c494b38
hyein0514 <hi020514@gmail.com>
2026-07-13 17:32:07 +0900

    fix:알람 웹소켓 url 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 const ALERT_WS_URLS = {
-  development: "http://localhost:8081/ws",
-  production: "https://aims-factory.com/ws",
+  development: "/ws",
+  production: "/ws",
 } as const;
 
 export default defineConfig(({ mode }) => {
@@ -15,10 +15,12 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+
     define: {
       global: "globalThis",
       __ALERT_WS_URL__: JSON.stringify(alertWebSocketUrl),
     },
+
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
@@ -27,23 +29,27 @@ export default defineConfig(({ mode }) => {
 
     server: {
       proxy: {
-        // "/ws": {
-        //   target: "http://localhost:8081",
-        //   changeOrigin: true,
-        //   ws: true,
-        // },
+        "/ws": {
+          target: "http://localhost:8081",
+          changeOrigin: true,
+          ws: true,
+        },
+
         "/api/ai": {
           target: "http://localhost:8000",
           changeOrigin: true,
         },
+
         "/api/quality/inspection": {
           target: "http://localhost:8083",
           changeOrigin: true,
         },
+
         "/api/process": {
           target: "http://localhost:8082",
           changeOrigin: true,
         },
+
         "/api": {
           target: "http://localhost:8081",
           changeOrigin: true,
